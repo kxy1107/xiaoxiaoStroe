e1836d19fc50004071fa426a8b2af3c4cad641f3
kxy <573240722@qq.com>
2017-08-07 00:16:19 +0800

    update08070015

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 //app.js
+let util = require('utils/util.js');
 App({
-onLaunch: function() { 
-  var that = this;
+  onLaunch: function () {
+    var that = this;
     // Do something initial when launch.
     //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo;
+
+  },
+
+  getUserInfo: function () {
+    var that = this;
+    wx.login({
+      success: function (loginRes) {
+        wx.getUserInfo({
+          success: function (res) {
+            let code = loginRes.code;
+            let iv = res.iv;
+            let encryptedData = res.encryptedData;
+            let url = that.globalData.serverAddress + 'login';
+            let data = {
+              Code: code,
+              Iv: iv,
+              EncryptedData: encryptedData,
             }
-          })
-        }
-      })
+            util.HttpGet(url, data, "正在登录",
+              function (successRes) {
+                console.log(successRes);
+              },
+              function (failRes) {
+
+              });
+          }
+        })
+      }
+    })
   },
 
 
-  globalData:{
-    userInfo:null,
-    addressList:[],
-    otherAddressInfo:null,
+  globalData: {
+    userInfo: null,
+    serverAddress: 'http://localhost:8028/wx/',
+    otherAddressInfo: null,
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -98,5 +98,7 @@ Page({
   },
 
   onLoad: function () {
-  }
+    app.getUserInfo();
+  },
+  
 })
