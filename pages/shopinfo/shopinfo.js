5649fbc05f9f96eefaa6f0029ea651e13c9f3f9b
kxy1107 <573240722@qq.com>
2017-08-16 18:46:46 +0800

    201708151817

--- pages/shopinfo/shopinfo.js
+++ pages/shopinfo/shopinfo.js
@@ -4,77 +4,31 @@ var selectIndex;
 var attrIndex;
 var selectIndexArray = [
 ];
+var app = getApp();
+let util = require("../../utils/util.js");
+let shopID = "";
 Page({
   data: {
     indicatorDots: "true",//是否显示面板指示点
     autoplay: "true",//是否自动切换
     interval: "5000",//自动切换时间间隔
     duration: "1000",//滑动动画时长
-    animationData: {},
-    imgUrls: [
-      "../../img/shop_info_one1.jpg",
-      "../../img/shop_info_one2.jpg",
-      "../../img/shop_info_one3.jpg",
-      "../../img/shop_info_one4.jpg",
-      "../../img/shop_info_one5.jpg"
-    ],
     //商品详情
     shopInfo:
     {
-      shopTitle: "【年货节|买赠99元大礼包】Huawei/华为 畅享6全网通4G智能手机",
-      shopPrice: "￥1299",
-      courier: "快递:0.00",
-      sales: "月销7386笔",
-      address: "广东深圳"
+      shopTitle: "",//标题
+      shopPrice: "",//价格
+      shopViews:0,//浏览量
+      shopDescribe:"",//详情描述
+      bannerList:[],//轮播图
+      attributeList:[],//属性列表
     },
     //商品图片详情
-    shopInfoImgList: [
-      {
-        imgUrl: "../../img/shop_info_one_list_img1.jpg",
-        height: "300rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img2.jpg",
-        height: "300rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img3.jpg",
-        height: "300rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img4.jpg",
-        height: "804rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img5.jpg",
-        height: "804rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img6.jpg",
-        height: "490rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img7.jpg",
-        height: "200rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img8.jpg",
-        height: "1000rpx",
-      },
-      {
-        imgUrl: "../../img/shop_info_one_list_img9.jpg",
-        height: "1000rpx",
-      },
-
-
-    ],
-
     isShowSelectInfo: true,//是否隐藏选择信息详情
-    shopBuyCount: 1,
+    shopSelectInfoHaveSelect:"已选",
     shopSelectInfo: {
       selectInfoImg: "../../img/shop_info_select1.jpg",//弹窗商品图片
       selectInfoprice: "￥1299",//弹窗商品价格
-      selectInfoInventory: "库存3137件",//弹窗商品库存
       shopSelectInfoHaveSelect: "已选： ",
       selectInfoAttributeList: [
         {
@@ -224,9 +178,6 @@ Page({
 
 
 
-
-
-
   },
   //显示弹窗
   showSelectInfo: function () {
@@ -285,21 +236,45 @@ Page({
         });
   },
 
+  getShopDetailInfo:function(){
+    let that = this;
+    let url = app.globalData.serverAddress + 'getShopInfoDetail';
+    let data = {
+      UserNo: app.globalData.userInfo.UserNo,
+      ShopID: shopID
+
+    };
+    util.HttpGet(url, data, "正在加载",
+      function (successRes) {
+        if (successRes.Code == 1) {
+          that.setData({
+            shopInfo: successRes.ShopInfoList
+          });
+        }
+
+      },
+      function (failRes) {
+
+      });
+  },
+
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    shopID = options.id;
+    this.getShopDetailInfo();
 
-    shopDialogInfo = this.data.shopSelectInfo;
-    var name = "";
-    var size = shopDialogInfo.selectInfoAttributeList.length;
-    for (var i = 0; i < size; i++) {
-      selectIndexArray.push({ key: i, value: shopDialogInfo.selectInfoAttributeList[i].AttributeNameList[0].Name });
-      name += ' "' + selectIndexArray[i].value + '" ';
-    }
+    // shopDialogInfo = this.data.shopSelectInfo;
+    // var name = "";
+    // var size = shopDialogInfo.selectInfoAttributeList.length;
+    // for (var i = 0; i < size; i++) {
+    //   selectIndexArray.push({ key: i, value: shopDialogInfo.selectInfoAttributeList[i].AttributeNameList[0].Name });
+    //   name += ' "' + selectIndexArray[i].value + '" ';
+    // }
 
-    shopDialogInfo.shopSelectInfoHaveSelect = "已选:" + name;
-    this.setData({
-      shopSelectInfo: shopDialogInfo,
-    });
+    // shopDialogInfo.shopSelectInfoHaveSelect = "已选:" + name;
+    // this.setData({
+    //   shopSelectInfo: shopDialogInfo,
+    // });
 
     console.log("shopinfo:" + options)
   },
